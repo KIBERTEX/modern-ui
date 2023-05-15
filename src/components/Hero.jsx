e2e33a5d809c3fd1edc15cb57ba163fb8b85096d
Diana Salmenbayeva <salmebayevadiana@gmail.com>
2023-05-15 21:01:45 +0200

    in mid commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,14 @@
-
-const Hero = () => {
-  return (
-    <div>Hero</div>
+import style from '../style';
+import { discount, robot } from '../assets';
+const Hero = () => (
+  <section id ="home" className={`flex md:flex-row flex-col ${style.paddingY}`}>
+    <div className={`flex-1 ${style.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
+      <div className="flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2 ">
+        <img src={discount} alt="discount" className="w-[32px] h-[32px]"/>
+      </div>
+    </div>
+  </section>
   )
-}
+
 
 export default Hero
